cbdcd2a6e83e6c15b07ec8dca0ae8801a8423ade
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-06-16 11:34:54 -0300

    Seeding for table games

--- src/main/java/com/JoaoMarcos/dsList/classes/Game.java
+++ src/main/java/com/JoaoMarcos/dsList/classes/Game.java
@@ -25,14 +25,18 @@ public class Game implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-    
+
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     @Override
