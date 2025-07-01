07a29bf3fdab9ed049cc5a90b5fd0486b518ee03
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-06-30 22:04:13 -0300

    Adicionei mais um tratamento de exceção

--- src/main/java/com/JoaoMarcos/dsList/services/GameListService.java
+++ src/main/java/com/JoaoMarcos/dsList/services/GameListService.java
@@ -1,4 +1,5 @@
 package com.JoaoMarcos.dsList.services;
+
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import com.JoaoMarcos.dsList.classes.GameList;
 import com.JoaoMarcos.dsList.projections.GameMinProjection;
 import com.JoaoMarcos.dsList.repositories.GameListRepository;
 import com.JoaoMarcos.dsList.repositories.GameRepository;
+import com.JoaoMarcos.dsList.services.exceptions.IndexOfBoundsException;
 import com.JoaoMarcos.dsList.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,31 +36,55 @@ public class GameListService {
 
    @Transactional(readOnly = true)
    public GameListDto findById(Long id) {
-      Optional<GameList> op = gameListRepository.findById(id);
-
-      if (op.isPresent()) {
-        GameList gameList = op.get();
-        return  new GameListDto(gameList); 
+      if (id <= 0) {
+         throw new IndexOfBoundsException("Id game list out of valid range");
       } else {
-         throw new ResourceNotFoundException(id);
+         Optional<GameList> op = gameListRepository.findById(id);
+
+         if (op.isPresent()) {
+            GameList gameList = op.get();
+            return new GameListDto(gameList);
+         } else {
+            throw new ResourceNotFoundException(id);
+         }
       }
    }
 
    @Transactional
-   public void replacementPositionGame(int sourceIndex, int destinationIndex, Long game_list_id) {
+   public void replacementPositionGame(int sourceIndex, int destinationIndex, 
+     Long game_list_id) {
 
-      // Todo este trecho eu estou manipulando a lista em memória
-      List<GameMinProjection> list = gameRepository.searchByList(game_list_id);
-      GameMinProjection obj = list.get(sourceIndex);
-      list.remove(sourceIndex);
-      list.add(destinationIndex, obj);
-      // Fim do trecho que manipula a lista em memória
+      List<GameMinProjection> list = null;
+      GameMinProjection obj = null;
 
-      int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
-      int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+      Optional<GameList> listDto = gameListRepository.findById(game_list_id);
 
-      for (int i = min; i <= max; i++) {
-         gameListRepository.updateBelongingPosition(game_list_id, list.get(i).getId(), i);
+      if (listDto.isPresent()) {
+         // Todo este trecho eu estou manipulando a lista em memória
+         list = gameRepository.searchByList(game_list_id);
+         if ((sourceIndex <= list.size()) && (destinationIndex <= list.size() )) {
+
+            obj = list.get(sourceIndex);
+            list.remove(sourceIndex);
+            list.add(destinationIndex, obj);
+
+            // Fim do trecho que manipula a lista em memória
+            int min = sourceIndex < destinationIndex ? 
+              sourceIndex : destinationIndex;
+            int max = sourceIndex < destinationIndex ? 
+              destinationIndex : sourceIndex;
+
+            for (int i = min; i <= max; i++) {
+              gameListRepository.updateBelongingPosition(game_list_id, 
+              list.get(i).getId(), i);
+            }
+         }else{
+            throw new IndexOfBoundsException("Source index or destination index "+
+             "out of valid range");
+         }
+      }else{
+         throw new IndexOfBoundsException("Game list id out of valid range");
+
       }
    }
 }
