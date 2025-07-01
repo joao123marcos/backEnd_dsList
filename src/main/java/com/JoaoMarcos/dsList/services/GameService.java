2691230ae9f23a859540fbfef1e5f742d26dce9d
“joaomarcos” <Joaomarcos.spires@gmail.com>
2025-07-01 11:29:58 -0300

    Ajustando mais algumas exceções

--- src/main/java/com/JoaoMarcos/dsList/services/GameService.java
+++ src/main/java/com/JoaoMarcos/dsList/services/GameService.java
@@ -12,39 +12,58 @@ import com.JoaoMarcos.dsList.DTO.GameDto;
 import com.JoaoMarcos.dsList.classes.Game;
 import com.JoaoMarcos.dsList.projections.GameMinProjection;
 import com.JoaoMarcos.dsList.repositories.GameRepository;
+import com.JoaoMarcos.dsList.services.exceptions.IndexOfBoundsException;
 import com.JoaoMarcos.dsList.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class GameService {
-    
+
    @Autowired
    public GameRepository gameRepository;
 
-   @Transactional(readOnly = true)
-   public GameByIdDto fingById(Long id){
-      Optional<Game> optionalGame = gameRepository.findById(id);
+   @Autowired
+   public GameListService gameListService;
 
-      if (optionalGame.isPresent()) {
-         Game game = optionalGame.get();
-         return new GameByIdDto(game);
+   @Transactional(readOnly = true)
+   public GameByIdDto fingById(Long id) {
+      if (id <= 0) {
+         throw new IndexOfBoundsException("Game id out of valid range");
       } else {
-         throw new ResourceNotFoundException(id);
+         Optional<Game> optionalGame = gameRepository.findById(id);
+
+         if (optionalGame.isPresent()) {
+            Game game = optionalGame.get();
+            return new GameByIdDto(game);
+
+         } else {
+            throw new ResourceNotFoundException(id);
+         }
       }
    }
-   
-   @Transactional(readOnly = true)
-   public List<GameDto> findAll(){
-    List<Game> result = gameRepository.findAll();
-    return result.stream().map(x -> new GameDto(x)).toList();
 
-    /*Eu tô pegando cada elemento dentro da lista de games e passando ele como
-     * argumento para o construtor do GameDto, transformando assim um Game em
-     * GameDto*/
+   @Transactional(readOnly = true)
+   public List<GameDto> findAll() {
+      try {
+         /*
+          * Eu tô pegando cada elemento dentro da lista de games e passando ele
+          * como argumento para o construtor do GameDto, transformando assim um
+          * Game em GameDto
+          */
+
+         List<Game> result = gameRepository.findAll();
+         return result.stream().map(x -> new GameDto(x)).toList();
+
+      } catch (RuntimeException e) {
+         System.out.println(e.getMessage());
+         return null;
+      }
    }
 
    @Transactional(readOnly = true)
-   public List<GameDto> findByList(Long listId){
-    List<GameMinProjection> result = gameRepository.searchByList(listId);
-    return result.stream().map(x -> new GameDto(x)).toList();
+   public List<GameDto> findByList(Long listId) {
+
+      gameListService.findById(listId);
+      List<GameMinProjection> result = gameRepository.searchByList(listId);
+      return result.stream().map(x -> new GameDto(x)).toList();
    }
 }
